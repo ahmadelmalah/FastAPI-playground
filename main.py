693d4844df4f087381d375561acd7d98539c05cc
Ahmad Elmalah <ahmedelmalah@hotmail.com>
2024-01-13 23:50:59 +0200

    Exception handling

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from fastapi import FastAPI, Depends
+import uvicorn
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI()
-basic = HTTPBasic()
 
 @app.get("/")
 def read_root():
     return "Root"
 
+# Basic Auth with username and password
 @app.get("/auth")
-def get_user(
-    creds: HTTPBasicCredentials = Depends(basic)):
+def get_user(creds: HTTPBasicCredentials = Depends(HTTPBasic())):
     return {"username": creds.username, "password": creds.password}
+
+# Exception handling
+@app.get("/exception")
+def get_exeption():
+    raise HTTPException(status_code=401, detail="Something went wrong!")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
