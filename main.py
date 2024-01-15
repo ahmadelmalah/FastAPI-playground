e2a65043bdca1030b80071a72cbf11d4fdc16acf
Ahmad Elmalah <ahmedelmalah@hotmail.com>
2024-01-15 16:39:54 +0200

    in memory database

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI()
 
+in_memory_db = []
+
 @app.get("/")
 def read_root():
     return "Root"
@@ -17,6 +19,15 @@ def get_user(creds: HTTPBasicCredentials = Depends(HTTPBasic())):
 @app.get("/exception")
 def get_exeption():
     raise HTTPException(status_code=401, detail="Something went wrong!")
+
+@app.get("/db")
+def get_db():
+    return in_memory_db
+
+@app.post("/db")
+def post_db(item: str):
+    in_memory_db.append(item)
+    return in_memory_db
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
