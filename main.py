a79b79ad61cb5ee696a74fcfdd502309c1ff30d3
Ahmad Elmalah <ahmedelmalah@hotmail.com>
2025-01-03 00:41:58 +0200

    Update basic functions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from pydantic import BaseModel
 from auth.hash_password import HashPassword
 import sys
 
@@ -8,9 +9,11 @@ app = FastAPI()
 
 in_memory_db = []
 
+
 @app.get("/")
 def read_root():
     return "Root"
+
 
 @app.get("/password-hash")
 def read_test():
@@ -22,24 +25,38 @@ def read_test():
     res["verify_password"] = HashPassword().verify_password(password, hashed_password)
     return res
 
+
 # Basic Auth with username and password
 @app.get("/auth")
 def get_user(creds: HTTPBasicCredentials = Depends(HTTPBasic())):
     return {"username": creds.username, "password": creds.password}
+
 
 # Exception handling
 @app.get("/exception")
 def get_exeption():
     raise HTTPException(status_code=401, detail="Something went wrong!")
 
-@app.get("/db")
+
+@app.get("/list")
 def get_db():
     return in_memory_db
 
-@app.post("/db")
+
+@app.get("/search")
 def post_db(item: str):
-    in_memory_db.append(item)
-    return in_memory_db
+    return item in in_memory_db
+
+
+class Item(BaseModel):
+    item: str
+
+
+@app.post("/add")
+def post_db(item: Item):
+    in_memory_db.append(item.item)
+    return item.item
+
 
 if __name__ == "__main__":
     sys.dont_write_bytecode = True
